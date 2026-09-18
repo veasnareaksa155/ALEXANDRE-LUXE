ba21c8bb33cd575f5c485d31a65327b68afdd11a
Veasna Reaksa <veasnareaksa155@gmail.com>
2026-09-18 16:57:58 +0700

    Auto seed database on startup with admin user, categories and products

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,27 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seed SuperAdmin User
+        User::updateOrCreate(
+            ['email' => 'admin@alexandreluxe.com'],
+            [
+                'name' => 'SuperAdmin Alexandre',
+                'password' => Hash::make('password123'),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            $this->call([
-                CategorySeeder::class,
-                ProductSeeder::class,
-            ])
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
